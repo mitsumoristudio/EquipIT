3eaa9025c85617656a871f93f4df819bb232d903
Satoshi550 <78008562+Satoshi550@users.noreply.github.com>
2024-06-28 17:09:44 +0900

    Initial Commit

---
+++ Equipment Log/Equipment_LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Equipment_LogApp.swift
+//  Equipment Log
+//
+//  Created by Satoshi Mitsumori on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Equipment_LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
